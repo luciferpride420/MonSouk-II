7c3277fcd8e6272579e7edbb3d4a41ee03c96be5
@y4ssine20 <yassinechraibi20@gmail.com>
2024-08-28 16:01:18 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+/* Start SignIN/Up */
+
+const sign_in_btn = document.querySelector('#sign-in-button');
+const sign_up_btn = document.querySelector('#sign-up-button');
+const container = document.querySelector('.container');
+
+sign_up_btn.addEventListener('click', () => {
+    container.classList.add('sign-up-mode');
+});
+
+sign_in_btn.addEventListener('click', () => {
+    container.classList.remove('sign-up-mode');
+});
+
+
+/* End  SignIN/Up */
+
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+/* Start hide and display navbar when on tablet or phone */
 const nav = document.getElementById('navbar');
-
 document.getElementById('bar').addEventListener('click', function() {
     nav.classList.toggle('active');
 });
+/* End hide and display navbar when on tablet or phone */
